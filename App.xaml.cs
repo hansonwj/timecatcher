d3a4e32e4f1be4e71cca56a3764a4dfa2cbdd8e3
Jake <hansonwj@protonmail.com>
2023-07-23 00:26:22 +1200

    Set manual/auto and inactive

--- App.xaml.cs
+++ App.xaml.cs
@@ -26,6 +26,8 @@ using Newtonsoft.Json;
 using System.Diagnostics.Metrics;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth.Advertisement;
+using Microsoft.Win32;
+using Windows.UI.Notifications.Management;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -46,28 +48,121 @@ namespace timecatcher
             this.InitializeComponent();
         }
 
+        // Import the necessary function from user32.dll
+        [DllImport("user32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        private static bool IsInactive()
+        {
+            // Get the last input information
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                // Calculate the time since the last input event in milliseconds
+                uint idleTime = (uint)Environment.TickCount - lastInputInfo.dwTime;
+
+                // If the system has been idle for more than the specified time (15 minutes in this case),
+                // it is considered locked
+                return idleTime > 900000; // 15 minutes = 900,000 milliseconds
+            }
+
+            return false;
+        }
+
         /// <summary>
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
+
+        private void CreateCsvFile(string FilePath, string Headings)
+        {
+            try
+            {
+                using (StreamWriter fileWriter = new StreamWriter(FilePath))
+                {
+                    fileWriter.WriteLine(Headings);
+                }
+
+                Debug.WriteLine("'timeEntries.csv' file has been created with headings.");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("An error occurred while creating the file: " + ex.Message);
+            }
+        }
+
+        public static class Globals
+        {
+            public static String CurrentClient = ""; // Modifiable
+        }
+
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
-            m_window.Activate(); //// Create process & dispatcher
+            //m_window.Activate(); //// Create process & dispatcher
 
             AppNotificationManager notificationManager = AppNotificationManager.Default;
             notificationManager.NotificationInvoked += NotificationManager_NotificationInvoked;
             notificationManager.Register();
             // Create a 30 min timer 
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string _configJson = File.ReadAllText(System.IO.Path.Combine(docPath, "timecatcher", "config.json"));
+            string ConfigDir = System.IO.Path.Combine(docPath, "timecatcher");
+            string ConfigFile = System.IO.Path.Combine(ConfigDir, "config.json");
+            string TimeEntriesFile = System.IO.Path.Combine(ConfigDir, "timeentries.csv");
+            string _configJson = "";
+
+            if (!Directory.Exists(ConfigDir))
+            {
+                Directory.CreateDirectory(ConfigDir);
+            }
+
+            if (File.Exists(ConfigFile))
+            {
+                _configJson = File.ReadAllText(ConfigFile);
+            } else
+            {
+                // Create a new Config object with the default values
+                var configObject = new
+                {
+                    Clients = new[]
+                    {
+                        new { name = "Admin" }
+                    },
+                    Schedule = 15,
+                    TimerEnabled = true
+                };
+
+                // Serialize the Config object to JSON
+                _configJson = JsonConvert.SerializeObject(configObject, Formatting.Indented);
+
+                    // Write the JSON to the config.json file
+                    File.WriteAllText(ConfigFile, _configJson);
+            }
+
+            if (!File.Exists(TimeEntriesFile))
+            {
+                string Headings = "timestamp,client,project,notes,manual";
+               CreateCsvFile(TimeEntriesFile, Headings);
+            }
+
+            
+
             var _schedule = JsonConvert.DeserializeObject<ConfigData>(_configJson).schedule;
             var _timerEnabled = JsonConvert.DeserializeObject<ConfigData>(_configJson).timerEnabled;
 
             var timer = new System.Timers.Timer(_schedule*1000*60);
 
             // Hook up the Elapsed event for the timer.
-            timer.Elapsed += (sender, e) => OnTimedEvent(sender, e, notificationManager); ;
+            timer.Elapsed += (sender, e) => OnTimedEvent(sender, e, notificationManager);
 
             timer.Enabled = _timerEnabled;
             
@@ -86,11 +181,31 @@ namespace timecatcher
 
         private Window m_window;
 
-
-
         private async static void OnTimedEvent(object sender, EventArgs e, AppNotificationManager notificationManager)
         {
+            // Create a time entry for 15 mins ago and clear out existing notifications
+            // if they haven't been clicked
+            IEnumerable<AppNotification> notifications = await notificationManager.GetAllAsync();
+            if (notifications.Count() > 0)
+            {
+                var entry = new TimeEntry();
+
+                if (IsInactive())
+                {
+                    entry.Client = "Inactive";
+                }
+                else {
+                    entry.Client = Globals.CurrentClient;
+                }
+                entry.Project = "";
+                entry.Notes = "";
+                entry.Datetime = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                entry.Manual = "False";
+                ProcessNewTimeEntry(entry);
+            }
             await notificationManager.RemoveAllAsync();
+
+            // Create new notification
             CreateTimeEntryNotification();
         }
 
@@ -117,7 +232,7 @@ namespace timecatcher
             public string name { get; set; }
         }
 
-        private static async void CreateTimeEntryNotification()
+        private static void CreateTimeEntryNotification()
         {
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string _configJson = File.ReadAllText(System.IO.Path.Combine(docPath, "timecatcher", "config.json"));
@@ -134,6 +249,8 @@ namespace timecatcher
             {
                 clientcombo.AddItem(client.name, client.name);
             }
+           
+            clientcombo.SelectedItem = Globals.CurrentClient;
             appNotificationBuilder.AddComboBox(clientcombo);
 
             appNotificationBuilder.AddTextBox("Notes", "Notes", "");
@@ -180,14 +297,18 @@ namespace timecatcher
                     entry.Client = args.UserInput["Client"];
                     entry.Project = "";
                     entry.Notes = args.UserInput["Notes"];
-                    entry.Datetime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                    entry.Datetime = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                    entry.Manual = "True";
                     ProcessNewTimeEntry(entry);
+                    Globals.CurrentClient = args.UserInput["Client"];
                 }
                 else {
                     Debug.WriteLine("Opening Manager");
                     LaunchAndBringToForegroundIfNeeded();
                 }
+
             });
+
         }
 
         public struct TimeEntry
@@ -196,6 +317,7 @@ namespace timecatcher
             public string Project;
             public string Notes;
             public string Datetime;
+            public string Manual;
         };
 
         public static void ProcessNewTimeEntry(TimeEntry timeentry)
@@ -206,7 +328,7 @@ namespace timecatcher
             // Append text to an existing file named "WriteLines.txt".
             using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "timecatcher", "timeentries.csv"), true))
             {
-                outputFile.WriteLine($"{timeentry.Client},{timeentry.Project},{timeentry.Notes},{timeentry.Datetime}");
+                outputFile.WriteLine($"{timeentry.Datetime},{timeentry.Client},{timeentry.Project},{timeentry.Notes},{timeentry.Manual}");
             }
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # timecatcher
+
+## Packaging
+Create package signing certificate https://learn.microsoft.com/en-us/windows/msix/package/create-certificate-package-signing
+
+Package and Publish > Create packaged app https://learn.microsoft.com/en-us/windows/msix/package/packaging-uwp-apps
